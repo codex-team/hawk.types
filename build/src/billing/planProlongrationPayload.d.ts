6521fbef4faf33e953fda95d7f72d0c73e9fb76f
GeekaN <geekan@bk.ru>
2021-02-19 21:20:09 +0300

    Make fields required

--- build/src/billing/planProlongrationPayload.d.ts
+++ build/src/billing/planProlongrationPayload.d.ts
@@ -5,13 +5,13 @@ export interface PlanProlongationPayload {
     /**
      * Workspace Identifier
      */
-    workspaceId?: string;
+    workspaceId: string;
     /**
      * Id of the user making the payment
      */
-    userId?: string;
+    userId: string;
     /**
      * Workspace current plan id or plan id to change
      */
-    tariffPlanId?: string;
+    tariffPlanId: string;
 }
--- src/billing/planProlongrationPayload.ts
+++ src/billing/planProlongrationPayload.ts
@@ -5,15 +5,15 @@ export interface PlanProlongationPayload {
   /**
    * Workspace Identifier
    */
-  workspaceId?: string;
+  workspaceId: string;
 
   /**
    * Id of the user making the payment
    */
-  userId?: string;
+  userId: string;
 
   /**
    * Workspace current plan id or plan id to change
    */
-  tariffPlanId?: string;
+  tariffPlanId: string;
 }
