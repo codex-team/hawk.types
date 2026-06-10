c72111b06c3dce7749ee0ea931779d9d71a11c92
Dobrunia Kostrigin <48620984+Dobrunia@users.noreply.github.com>
2026-06-10 19:02:57 +0300

    init

---
+++ src/dbScheme/promoCodeUsage.ts
@@ -0,0 +1,32 @@
+import type { ObjectId } from 'bson';
+
+/**
+ * Promo code usage representation in DataBase
+ */
+export interface PromoCodeUsageDBScheme {
+  /**
+   * Promo code usage id
+   */
+  _id: ObjectId;
+
+  /**
+   * Applied promo code id
+   */
+  promoCodeId: ObjectId;
+
+  /**
+   * User who applied promo code
+   */
+  userId: string;
+
+  /**
+   * Workspace where promo code was applied
+   */
+  workspaceId: ObjectId;
+
+  /**
+   * Date when promo code was applied
+   * @example 2026-06-10T12:30:00.000Z
+   */
+  appliedAt: Date;
+}
